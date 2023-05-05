9d7493fcdf57ad79d5df03d459f1feb3f52312c5
maurodiaztesta <maurocandas@gmail.com>
2023-05-05 12:00:04 +0200

    Page Splash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Link, Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import './App.scss';
+import SplashPage from './pages/SplashPage/SplashPage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+    <Router>
+        <main>
+          <Routes>
+            <Route path="/" element={<SplashPage></SplashPage>}/>
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/SplashPage/SplashPage.jsx
@@ -0,0 +1,13 @@
+import "./SplashPage.scss"
+import React from 'react'
+
+const SplashPage = () => {
+  return (
+    <div className="padre">
+        <img className="padre__logo" src='/assets/img/logo/lucky_logo.png' alt='logo'></img>
+        <img className="padre__titulo" src='/assets/img/logo/lucky_titulo.png' alt='titulo'></img>
+    </div>
+  )
+}
+
+export default SplashPage
