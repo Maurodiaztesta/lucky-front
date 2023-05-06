bc5530ce33c2020d97819f355deb1193b190a39c
Alessandrotri <alezuz1988@gmail.com>
2023-05-06 14:06:07 +0200

    swiper component

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -1,15 +1,17 @@
+
 import "./SplashPage.scss"
 import React from 'react'
 
 const SplashPage = () => {
   return (
+    
     <a>
       <div className="padre">
         <img className="padre__logo" src='/assets/img/logo/lucky_logo.png' alt='logo'></img>
         <img className="padre__titulo" src='/assets/img/logo/lucky_titulo.png' alt='titulo'></img>
       </div>
-    </a>
-    
+    </a> 
+   
   )
 }
 
