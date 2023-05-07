ba2de9a124b3824943f30b24f48a2b532c6ab84f
rubenFdzC <rubenfdz1998@gmail.com>
2023-05-07 13:13:15 +0200

    NavBar linked with the routes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,16 @@
-// import { Link } from "react-router-dom";
 import React from 'react'
 import "./NavBar.css"
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <>
      <nav className='NavBar'>
-        <img className="NavLogo" src="../../assets/img/logo/home.png" alt="home"></img>
+       <Link to="/home"><img className="NavLogo" src="../../assets/img/logo/home.png" alt="home"></img></Link>
         <img className="NavLogo" src="../../assets/img/logo/NavMap.png" alt="map"></img>
-        <img className="NavLogo" src="../../assets/img/logo/mascota.png" alt="pets"></img>
-        <img className="NavLogo" src="../../assets/img/logo/perfil.png" alt="profile"></img>
-        <img className="NavLogo" src="../../assets/img/logo/assets.png" alt="assets"></img>
+        <Link to="/adopt"> <img className="NavLogo" src="../../assets/img/logo/mascota.png" alt="pets"></img></Link>
+       <Link to="/profile"><img className="NavLogo" src="../../assets/img/logo/perfil.png" alt="profile"></img></Link>
+       <Link to="/more"><img className="NavLogo" src="../../assets/img/logo/assets.png" alt="assets"></img></Link>
      </nav>
     </>
   )
